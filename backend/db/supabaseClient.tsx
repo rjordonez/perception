6381273ccc1026ee5302c406780b8721eae74142
Tavasya Ganpati <tavasyag@gmail.com>
2024-11-23 01:04:27 -0800

    set up env

---
+++ backend/db/supabaseClient.tsx
@@ -0,0 +1,10 @@
+// app.js or supabaseClient.js
+require('dotenv').config();
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log('Supabase client initialized!');
